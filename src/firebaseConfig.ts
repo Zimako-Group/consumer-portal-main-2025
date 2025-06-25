e5941bc3cb7c571a05fb170876958b7dc1fb3f15
Zimako-Dev <zimakodev@gmail.com>
2025-06-25 14:35:25 +0200

    secrets fix

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -8,14 +8,14 @@ import { getStorage, ref } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwb5v9FrGaEaRG86pHjxtPLwrKn1llB3s",
-  authDomain: "zimako-backend.firebaseapp.com",
-  projectId: "zimako-backend",
-  storageBucket: "zimako-backend.firebasestorage.app",
-  messagingSenderId: "833787391563",
-  appId: "1:833787391563:web:ff4cb0f78d73ba15c667ce",
-  measurementId: "G-YB7X3FBZE8",
-  databaseURL: "https://zimako-backend-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
